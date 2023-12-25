c39ffe6e4c6443d218f0de6afb1b3f370980e9d0
holikma <marekholik01@gmail.com>
2023-12-25 21:15:41 +0100

    update

--- Eshop/Eshop.cpp
+++ Eshop/Eshop.cpp
@@ -3,6 +3,32 @@
 
 Eshop::Eshop(QWidget* parent) : QMainWindow(parent) {
     ui.setupUi(this);
+	ui.Line_Name->setPlaceholderText("Name");
+	ui.Line_Surname->setPlaceholderText("Surname");
+    connect(ui.Button_LoadData, SIGNAL(clicked()), this, SLOT(LoadProducts()));
+
 }
 
 Eshop::~Eshop() {}
+
+void Eshop::LoadProducts() {
+
+}
+
+
+Product::Product(QString name, float price, int quantity) {
+	this->name = name;
+    this->price = price;
+    this->quantity = quantity;
+}
+
+Customer::Customer(QString Name, QString Surname, Cart cart) {
+	this->Name = Name;
+	this->Surname = Surname;
+	this->cart = cart;
+}
+
+Cart::Cart() {
+	products = {};
+	size = 0;
+}
--- Eshop/Eshop.h
+++ Eshop/Eshop.h
@@ -3,12 +3,48 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_Eshop.h"
 
+
+class Product {
+	private:
+		QString name;
+		float price;
+		int quantity;
+
+	public:
+		Product(QString name, float price, int quantity);
+};
+
+class Cart {
+	private:
+		int size;
+		QList<Product> products;
+
+	public:
+		Cart();
+};
+
+class Customer {
+	private:
+		QString Name;
+		QString Surname;
+		Cart cart;
+
+	public:
+		Customer() {};
+		Customer(QString name, QString Surname, Cart cart);
+};
+
 class Eshop : public QMainWindow{
-    Q_OBJECT
+	Q_OBJECT
+	private:
+		Customer customer;
+		Ui::EshopClass ui;
+
+	private slots:
+		void LoadProducts();
+
     public:
         Eshop(QWidget *parent = nullptr);
         ~Eshop();
-
-    private:
-        Ui::EshopClass ui;
 };
+
