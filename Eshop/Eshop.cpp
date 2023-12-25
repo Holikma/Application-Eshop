a50ef5c49f928ea74d914b88701ec14eafa3e83f
holikma <marekholik01@gmail.com>
2023-12-25 23:51:48 +0100

    update

--- Eshop/Eshop.cpp
+++ Eshop/Eshop.cpp
@@ -5,30 +5,69 @@ Eshop::Eshop(QWidget* parent) : QMainWindow(parent) {
     ui.setupUi(this);
 	ui.Line_Name->setPlaceholderText("Name");
 	ui.Line_Surname->setPlaceholderText("Surname");
-    connect(ui.Button_LoadData, SIGNAL(clicked()), this, SLOT(LoadProducts()));
+    connect(ui.Button_LoadData, SIGNAL(clicked()), this, SLOT(Set_Shop()));
 
 }
 
 Eshop::~Eshop() {}
 
-void Eshop::LoadProducts() {
+void Eshop::Set_Shop() {
+	QString filePath = QFileDialog::getOpenFileName(nullptr, "Open File",
+		QFileInfo(QCoreApplication::applicationDirPath()).dir().filePath("Data/produkty.txt"));
+	QFile file(filePath);
+	customer = Customer(ui.Line_Name->text(), ui.Line_Surname->text(), ui.Line_Budget->text().toFloat(), Cart());
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		qDebug() << "File not opened";
+		return;
+	}
 
+	QTextStream in(&file);
+	QString line = in.readLine();
+	Shop.resize(line.toInt());
+	line = in.readLine();
+	for (int i = 0; i < Shop.size(); i++) {
+		QStringList list = line.split(" ");
+		Product product(list[0].toInt(), list[1], list[2], list[3].toFloat(), list[4].toInt());
+		Shop[i] = product;
+		line = in.readLine();
+		qDebug() << Shop[i].GetId();
+	}
+	
+	file.close();
 }
 
 
-Product::Product(QString name, float price, int quantity) {
+Product::Product(int id, QString name, QString distributor, float price, int quantity) {
+	this->id = id;
 	this->name = name;
+	this->distributor = distributor;
     this->price = price;
     this->quantity = quantity;
 }
 
-Customer::Customer(QString Name, QString Surname, Cart cart) {
+Customer::Customer(QString Name, QString Surname, float money, Cart cart) {
 	this->Name = Name;
 	this->Surname = Surname;
+	this->money = money;
 	this->cart = cart;
 }
 
-Cart::Cart() {
-	products = {};
-	size = 0;
+void Customer::Add_To_Cart(Product product) {
+	cart.Add(product);
 }
+
+
+void Cart::Add(Product product) {
+	products.append(product);
+}
+
+void Cart::Print() {
+	for (int i = 0; i < products.size(); i++) {
+		qDebug() << products[i].GetId();
+		qDebug() << products[i].GetName().toStdString();
+		qDebug() << products[i].GetDistributor().toStdString();
+		qDebug() << products[i].GetPrice();
+		qDebug() << products[i].GetQuantity();
+	}
+}
+
--- Eshop/Eshop.h
+++ Eshop/Eshop.h
@@ -2,49 +2,67 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_Eshop.h"
+#include <QFileDialog>
+#include <QDir>
+#include <QVector>
 
 
 class Product {
 	private:
+		int id;
 		QString name;
+		QString distributor;
 		float price;
 		int quantity;
 
 	public:
-		Product(QString name, float price, int quantity);
+		Product() : id(), name(), distributor(), price(), quantity() {};
+		Product(int id, QString name, QString distributor, float price, int quantity);
+		int GetId() { return id; };
+		QString GetName() { return name; };
+		QString GetDistributor() { return distributor; };
+		float GetPrice() { return price; };
+		int GetQuantity() { return quantity; };
+		
 };
 
 class Cart {
 	private:
-		int size;
-		QList<Product> products;
+		QVector<Product> products;
 
 	public:
-		Cart();
+		Cart() : products() {};
+		void Add(Product product);
+		void Print();
 };
 
 class Customer {
 	private:
 		QString Name;
 		QString Surname;
+		float money;
 		Cart cart;
 
 	public:
-		Customer() {};
-		Customer(QString name, QString Surname, Cart cart);
+		Customer() : Name(), Surname(), money(), cart() {};
+		Customer(QString name, QString Surname, float money, Cart cart);
+		void Add_To_Cart(Product product);
+		Cart Get_Cart() { return cart; };
 };
 
 class Eshop : public QMainWindow{
 	Q_OBJECT
 	private:
 		Customer customer;
+		QVector<Product> Shop;
 		Ui::EshopClass ui;
 
 	private slots:
-		void LoadProducts();
+		void Set_Shop();
 
     public:
         Eshop(QWidget *parent = nullptr);
         ~Eshop();
+
 };
 
